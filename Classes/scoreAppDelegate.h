1865b4545b02bb8f37cc2318c3af39665a9cb3ca
poppyrr <poppyrr@gmail.com>
2011-05-21 18:44:16 +0800

    init

---
+++ Classes/scoreAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  scoreAppDelegate.h
+//  score
+//
+//  Created by poppy on 11-5-21.
+//  Copyright 2011 百度在线网络技术（北京）有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface scoreAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  score
+//
+//  Created by poppy on 11-5-21.
+//  Copyright 2011 百度在线网络技术（北京）有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
