1e43670a37d62262620886cbcffc1b5ab68eb73f
poppyrr <poppyrr@gmail.com>
2011-05-24 22:43:27 +0800

    add default tests

---
+++ scoreTests/scoreTests.h
@@ -0,0 +1,17 @@
+//
+//  scoreTests.h
+//  scoreTests
+//
+//  Created by poppy on 11-5-24.
+//  Copyright 2011年 百度在线网络技术（北京）有限公司. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface scoreTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ scoreTests/scoreTests.m
@@ -0,0 +1,33 @@
+//
+//  scoreTests.m
+//  scoreTests
+//
+//  Created by poppy on 11-5-24.
+//  Copyright 2011年 百度在线网络技术（北京）有限公司. All rights reserved.
+//
+
+#import "scoreTests.h"
+
+
+@implementation scoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in scoreTests");
+}
+
+@end
